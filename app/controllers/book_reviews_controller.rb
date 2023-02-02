6829e93fafe9a9a1359a3e5307a082934883e51f
hoang <hoanghua216@gmail.com>
2023-02-02 06:25:18 +0000

    Add list review screen

--- app/controllers/book_reviews_controller.rb
+++ app/controllers/book_reviews_controller.rb
@@ -23,9 +23,10 @@ class BookReviewsController < ApplicationController
   def create
     @book_review = BookReview.new(book_review_params)
     @book_review.user_id = current_user.id
+    url = "/books/" + @book_review.book_id.to_s
     respond_to do |format|
       if @book_review.save
-        format.html { redirect_to book_review_url(@book_review), notice: "Book review was successfully created." }
+        format.html { redirect_to url, notice: "Book review was successfully created." }
         format.json { render :show, status: :created, location: @book_review }
       else
         format.html { render :new, status: :unprocessable_entity }
